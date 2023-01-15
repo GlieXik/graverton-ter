8b285ff40068c159166ae5e771ef3cfbb16ed3a4
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-01-15 09:50:03 +0200

    fixed

--- js/form.js
+++ js/form.js
@@ -19,8 +19,38 @@ const handleSubmit = async (event) => {
 
   await axios
     .post("https://graverton-ter.onrender.com", formData)
-
-    .catch((e) => console.error(e))
+    .then(() => {
+      console.log("sended");
+      Toastify({
+        text: "Відправленно✅",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #2196f3)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    })
+    .catch((e) => {
+      console.error(e);
+      Toastify({
+        text: "Не відправленно⛔️",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #f42, #2196f3)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    })
     .finally(toggleModal());
 };
 
